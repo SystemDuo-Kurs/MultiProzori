bd0bc8f59fe00e30f8c63f5feac36bc10a8e6cf8
Dragan Pepic <d.pepic@hotmail.com>
2021-10-04 18:31:20 +0200

    Add project files.

---
+++ MultiProzori/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiProzori
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<Osoba> Osobe { get; set; } = new();
+		public MainWindow()
+		{
+			InitializeComponent();
+			dg.ItemsSource = Osobe;
+			
+		}
+
+		private void Kliik(object sender, RoutedEventArgs e)
+		{
+			Prozor2 p = new();
+			p.Owner = this;
+			p.ShowDialog();
+			if (p.SveOk)
+				Osobe.Add(p.DataContext as Osoba);
+
+		}
+	}
+}
---
+++ MultiProzori/Prozor2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MultiProzori
+{
+	/// <summary>
+	/// Interaction logic for Prozor2.xaml
+	/// </summary>
+	public partial class Prozor2 : Window
+	{
+		public bool SveOk;
+		public Prozor2()
+		{
+			InitializeComponent();
+			DataContext = new Osoba();
+		}
+
+		private void Unos(object sender, RoutedEventArgs e)
+		{
+			Osoba o = DataContext as Osoba;
+			if (!(string.IsNullOrEmpty(o.Ime) || string.IsNullOrWhiteSpace(o.Ime)))
+			{
+				o.Ime = o.Ime.Trim();
+				SveOk = true;
+				Close();
+			} else
+			{
+				MessageBox.Show("GRRRRRR");
+			}
+		}
+	}
+}
